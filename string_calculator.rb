c6b7f7166fc0dde6c4fbb9d36c5bc4f9425d4708
Manu Dwivedi <manu29.d@gmail.com>
2024-11-18 11:42:37 +0530

    Refactor: Scan instead of split with delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -6,14 +6,7 @@ class StringCalculator
   def add(input_string)
     return 0 if input_string.empty?
 
-    delimiter_regex = %r{^//([^\\n]+)\\n}
-    input_string.match(delimiter_regex)
-    input_string.gsub!(delimiter_regex, '')
-
-    delimiter = ::Regexp.last_match(1) || ','
-
-    input_string.gsub!('\n', delimiter)
-    numbers = input_string.split(delimiter).map(&:to_i)
+    numbers = input_string.scan(/-?\d+/).map(&:to_i)
     negative_numbers = numbers.select(&:negative?)
 
     raise(ArgumentError, "negative numbers not allowed #{negative_numbers.join(',')}") unless negative_numbers.empty?
